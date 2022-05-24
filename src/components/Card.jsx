2a3a973d851371b44d2a0c1a072473f760988e0c
Jordan Kimsey <jordankimsey@gmail.com>
2022-05-24 16:13:00 -0400

    task item design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -45,7 +45,7 @@ const Card = () => {
   return (
     <div className='container-sm mx-auto bg-white border rounded-md border-gray-300 shadow-md w-5/6 md:w-3/5 lg:w-2/6 text-left'>
       <div className='relative flex items-center text-gray-300 focus-within:text-gray-500'>
-        <FaChevronDown className='absolute ml-2'/>
+        <FaChevronDown className='absolute ml-2' />
         <input
           onChange={(event) => setInput('task', event.target.value)}
           value={formState.task}
@@ -54,21 +54,28 @@ const Card = () => {
         />
       </div>
       {/* <button onClick={addTodo}>Create Todo</button> */}
-      <TaskItem />
+      <div>
+        <TaskItem todo='Clean Laundry' />
+        <TaskItem todo='Workout' />
+        <TaskItem todo='Go grocery shopping' />
+      </div>
+
       {todos.map((todo, index) => (
         <div key={todo.id ? todo.id : index}>
           <p>{todo.task}</p>
           <p>{todo.isComplete}</p>
         </div>
       ))}
-      <ItemsLeft numberofitems={0} />
-      <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
-      </div>
-      <div>
-        <button>Clear Completed</button>
+      <div className='flex place-items-end'>
+        <ItemsLeft numberofitems={0} />
+        <div>
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+        <div>
+          <button>Clear Completed</button>
+        </div>
       </div>
     </div>
   );
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {FaCheck} from 'react-icons/fa'
 
-const TaskItem = () => {
+const TaskItem = ({todo}) => {
   return (
-    <div>TaskItem</div>
-  )
-}
+    <div className='p-2 border-b border-gray-100 flex items-center'>
+      <div>
+        <button className='border border-gray-300 rounded-full p-2 text-green-500'>
+          <FaCheck />{' '}
+        </button>
+          </div>
+          <p className='pl-2'>{ todo}</p>
+    </div>
+  );
+};
 
-export default TaskItem
+export default TaskItem;
