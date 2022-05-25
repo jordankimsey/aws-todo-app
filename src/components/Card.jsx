595e8cbdbbfb0b25df3f85cd8a193b4e48a41084
Jordan Kimsey <jordankimsey@gmail.com>
2022-05-25 12:11:11 -0400

    Added update graphql logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,73 +12,73 @@ const Card = () => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState([]);
 
-
-
-
   useEffect(() => {
-    // fetchTodos();
+    fetchTodos();
   }, []);
-
+console.log(todos)
   function setInput(key, value) {
     setFormState({ ...formState, [key]: value });
   }
 
-  //   async function fetchTodos() {
-  //     try {
-  //       const todoData = await API.graphql(graphqlOperation(listTodos));
-  //       const todos = todoData.data.listTodos.items;
-  //       setTodos(todos);
-  //     } catch (err) {
-  //       console.log('error fetching todos');
-  //     }
-  //   }
+    async function fetchTodos() {
+      try {
+        const todoData = await API.graphql(graphqlOperation(listTodos));
+        const todos = todoData.data.listTodos.items;
+        setTodos(todos);
+      } catch (err) {
+        console.log('error fetching todos');
+      }
+    }
 
-  // async function addTodo() {
-  //   try {
-  //     if (!formState.task) return;
-  //     const todo = { ...formState };
-  //     setTodos([...todos, todo]);
-  //     setFormState(initialState);
-  //     await API.graphql(graphqlOperation(createTodo, { input: todo }));
-  //   } catch (err) {
-  //     console.log('error creating todo:', err);
-  //   }
-  // }
+  async function addTodo() {
+    try {
+      if (!formState.task) return;
+      const todo = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+    } catch (err) {
+      console.log('error creating todo:', err);
+    }
+  }
 
   return (
-    <div className='container-sm mx-auto relative h-64 bg-white border rounded-sm border-gray-300 shadow-md w-5/6 md:w-3/5 lg:w-2/6 text-left'>
+    <div className='container-sm mx-auto relative h-3/6 bg-white border rounded-sm border-gray-300 shadow-md w-5/6 md:w-3/5 lg:w-2/6 text-left'>
       <div className='relative flex items-center text-gray-300 focus-within:text-gray-500'>
         <FaChevronDown className='absolute ml-2' />
         <input
           onChange={(event) => setInput('task', event.target.value)}
+          onKeyPress={(e) => e.key === 'Enter' && addTodo()}
           value={formState.task}
           placeholder='What needs to be done?'
-          className='w-full cursor-pointer p-3 border-b-2 pl-8'
+          className='w-full cursor-pointer p-3 border-b-2 pl-8 focus:outline-none'
         />
-      </div>
-      <div className='flex flex-col'>
-        <TaskItem todo='Clean Laundry' isComplete={true} />
-        <TaskItem todo='Workout' />
-        <TaskItem todo='Go grocery shopping' />
       </div>
 
       {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index}>
-          <p>{todo.task}</p>
-          <p>{todo.isComplete}</p>
+        <div className='flex flex-col' key={todo.id ? todo.id : index}>
+          <TaskItem
+            ID={todo.id}
+            todo={todo.task}
+            isComplete={todo.isComplete}
+          />
         </div>
       ))}
       <div className='text-gray-300 flex justify-between border-t absolute bottom-0 left-0 right-0 border-gray-100 p-3'>
         <ItemsLeft numberofitems={3} />
         <div className='flex'>
-          <div className='selected hover:text-black cursor-pointer'>All</div>
-          <div className='hover:text-black pl-3 pr-3 cursor-pointer'>
+          <button className='pr-2 pl-2 hover:text-slate-400 active:border'>
+            All
+          </button>
+          <button className='pr-2 pl-2 hover:text-slate-400 active:border'>
             Active
-          </div>
-          <div className='hover:text-black cursor-pointer'>Completed</div>
+          </button>
+          <button className='pr-2 pl-2 hover:text-slate-400 active:border'>
+            Completed
+          </button>
         </div>
         <div>
-          <button className='hover:text-black'>Clear Completed</button>
+          <button className='hover:text-slate-400'>Clear Completed</button>
         </div>
       </div>
     </div>
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
-import {FaCheck} from 'react-icons/fa'
+import React, {useState} from 'react';
+import { FaCheck } from 'react-icons/fa';
+import { API, graphqlOperation } from 'aws-amplify';
+import { updateTodo, deleteTodo } from '../graphql/mutations';
 
-const TaskItem = ({ todo, isComplete }) => {
-  
+
+const TaskItem = ({ todo, isComplete, ID}) => {
+  const [toggleComplete, setToggleComplete] = useState(isComplete);
+
+  const todoDetails = {
+    id: ID,
+    isComplete: toggleComplete,
+  };
+
+async function updateComplete() {
+  try {
+    setToggleComplete(!toggleComplete)
+    await API.graphql(graphqlOperation(updateTodo, { input: todoDetails }));
+    console.log('Updated Completed!');
+  } catch (err) {
+    console.log({ err });
+  }
+}
+
+  // async function submitNewContact() {
+  //   try {
+  //     await API.graphql({
+  //       query: createContact,
+  //       variables: {
+  //         input: {
+  //           name: formState.name,
+  //           phone: formState,
+  //           phone,
+  //         },
+  //       },
+  //     });
+  //     console.log('New contact created!');
+  //   } catch (err) {
+  //     console.log({ err });
+  //   }
+  // }
+
+
+
 
   return (
-    <div className='p-2 border-b border-gray-100 flex items-center' >
+    <div className='p-2 border-b border-gray-100 flex items-center' onClick={(e) => updateComplete()}>
       <div>
         <button className={`border rounded-full p-2 ${isComplete ? 'border-green-500' : 'border-gray-300'}`}>
           {isComplete && <FaCheck className='text-green-500' />}
